c7774b1b82993444cfb8b2cc5684925bda872b2d
alenatsibets <alena.tsibets@gmail.com>
2025-01-25 15:00:19 +0200

    sound pause

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource MusicSource;
+    [SerializeField] AudioSource FXSource;
+
+    [Header("Audio Clips")]
+    [SerializeField] AudioClip background;
+
+    // [Header("Volume Settings")]
+    // public SoundManager soundManager;
+
+    public static AudioManager instance; 
+
+    private void Awake(){
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start(){
+        // if (PlayerPrefs.HasKey("musicVolume")){
+        //     soundManager.LoadValue();
+        // }
+        MusicSource.clip = background;
+        MusicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip){
+        FXSource.PlayOneShot(clip);
+    }
+
+    // public void PlayUI(){
+    //     UISource.PlayOneShot(ui);
+    // }
+
+    public void SoundControl(){
+        if(AudioListener.pause == true){
+            AudioListener.pause = false;
+        } else {
+            AudioListener.pause = true;
+        }
+    }
+}
