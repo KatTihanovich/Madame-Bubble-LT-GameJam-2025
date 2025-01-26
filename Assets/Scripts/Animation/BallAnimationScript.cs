f9d57b2c2140dbaea933e133252032ce113fb87d
alenatsibets <alena.tsibets@gmail.com>
2025-01-26 06:19:49 +0200

    added animations

---
+++ Assets/Scripts/Animation/BallAnimationScript.cs
@@ -0,0 +1,17 @@
+// using UnityEngine;
+// using Spine.unity;
+
+// public class BallAnimationScript : MonoBehaviour
+// {
+//     public SkeletonGraphic skeletonGraphic;
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
